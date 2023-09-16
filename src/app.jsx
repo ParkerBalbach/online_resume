2c96d13af16dba1056b1566e6efeb12743e75f60
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-15 19:41:13 -0600

    Rendered components and fixed education.jsx import

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Header from './components/header/header'
+import About from './components/about/about'
+import Workhistory from './components/workhistory/workhistory'
+import Education from './components/education/education.jsx'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 const app = () => {
   return (
-    <div>app</div>
+    <>
+      <Header />
+      <About />
+      <Education />
+      <Workhistory />
+      <Contact />
+      <Footer />
+    
+    </>
   )
 }
 
