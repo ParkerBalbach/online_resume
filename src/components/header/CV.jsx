00216940c526bca9bceba85c0fe59dcfff646b11
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-17 18:10:36 -0600

    Added download functionality to resume button

--- src/components/header/CV.jsx
+++ src/components/header/CV.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../images/Parker Balbach Resume.pdf'
+import RES from '../../images/Parker Balbach Resume.pdf'
 
 const CV = () => {
   return (
     <div className='cv'>
-        <a href="">Download Resume</a>
-        <a href="#contact">Contact Me</a>
+        <a href={RES} download className='btn'>Download Resume</a>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
 
     </div>
   )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import RES from './CV'
 
 const header = () => {
   return (
@@ -7,6 +8,7 @@ const header = () => {
       <div className="container header__container"></div>
         <h1>Parker Balbach</h1>
         <h5 className="text-light">Software Engineer</h5>
+        <RES />
     </header>
   )
 }
