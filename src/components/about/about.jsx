454fe2abd83f6b646f054e07e8f44927c1a9feb6
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-18 20:00:27 -0600

    Added placeholder for my picture

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './about.css'
+import MYPIC from '../../images/placeholder_image.png'
 
 const about = () => {
   return (
-    <div>about</div>
+    <section id='about'>
+      <h5>Career Objective</h5>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={MYPIC} alt="Me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
