6320df5f122363f718c47cedabb69854e10b8eac
ParkerBalbach <70546594+ParkerBalbach@users.noreply.github.com>
2023-09-19 18:39:51 -0600

    15 make a prototype of the about component (#20)
    * Added placeholder for my picture
    
    * Added card & added icon functionality
    
    * Decided to use 3 cards and added specific icons
    
    * Got rid of the space in import for FaTools
    
    * Decided to use 6+ years for experience
    
    * Added contact button
    
    * Made Languages and Tools more specific
    
    * Added formating and animation for about me picture
    
    * Created basic formatting for about cards
    
    * Added css for the about cards
    
    * added paragraph css
    
    * Hated Career Objective by itself added About Me
    
    * Changed the cards to have 1 word for formatting
    
    * Changed fontsize of cards to be a bit larger
    
    * Changed the formatting so everythings aligned
    
    * Added CSS comments to follow professional dev

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import MYPIC from '../../images/placeholder_image.png'
+import {FaBusinessTime} from 'react-icons/fa'
+import {MdLanguage} from 'react-icons/md'
+import {FaTools} from 'react-icons/fa'
 
 const about = () => {
   return (
-    <div>about</div>
+    <section id='about'>
+      <h5>About Me</h5>
+      <h2>Career Objective</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={MYPIC} alt="Me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaBusinessTime className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6+ Years</small>
+            </article>
+            <article className='about__card'>
+              <MdLanguage className='about__icon'/>
+              <h5>Languages</h5>
+              <small>10+</small>
+            </article>
+            <article className='about__card'>
+              <FaTools className='about__icon'/>
+              <h5>Tools</h5>
+              <small>10+</small>
+            </article>
+          </div>
+
+          <p>
+          Recent Computer Science graduate with over a year of hands-on experience building and testing applications using multiple tools and programming languages. 
+          Experienced with several programming languages (Python, React, Java, JavaScript, CSS, HTML, PHP, Kotlin, C, C#, C++, and SQL). Experienced with working in 
+          an agile team environment; as well as using a test driven development process. Worked with/as all roles in a Scrum system. Have worked with several different 
+          tools and frameworks(MySQL, Git, AWS, Firebase, Maven, Spring Boot, Docker, Apache Spark, and Jira) Seeking to use my development experience in an entry-level position.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Contact Information</a> 
+        </div>
+      </div>
+    </section>
   )
 }
 
